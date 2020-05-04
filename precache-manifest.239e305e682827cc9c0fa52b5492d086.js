07ffd3751023581fa702508fa97bc3b7bcaf3649
Nicolas Martinez <nicolas.martinez@noblypos.com>
2020-05-04 19:15:39 -0300

    Updates

---
+++ precache-manifest.239e305e682827cc9c0fa52b5492d086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774500480111051de3011d348e4ec159",
+    "url": "/react-wizard-builder/index.html"
+  },
+  {
+    "revision": "c775cffa64c6f27cd151",
+    "url": "/react-wizard-builder/static/css/main.b926a3cc.chunk.css"
+  },
+  {
+    "revision": "9394317af1b76d95aa9e",
+    "url": "/react-wizard-builder/static/js/2.727106e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-wizard-builder/static/js/2.727106e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c775cffa64c6f27cd151",
+    "url": "/react-wizard-builder/static/js/main.5fcda72b.chunk.js"
+  },
+  {
+    "revision": "005e6dc0e02b79640b3a",
+    "url": "/react-wizard-builder/static/js/runtime-main.f4410440.js"
+  }
+]);
